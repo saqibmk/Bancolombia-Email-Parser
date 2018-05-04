df6a48d961b9b1a4330cca6ad68cdad421eb3fb6
Saqib Khan <saqibmk@users.noreply.github.com>
2018-05-04 12:16:45 -0500

    Cleanup and added first summary component

---
+++ src/client/src/components/Summary.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Summary extends Component {
+  render() {
+    return (
+      <div>
+        <p> Total Spent $1.000.000,00</p>
+        <p> Debit $1.000.000,00</p>
+        <p> Credit $1.000.000,00</p>
+        <p> Payments $1.000.000,00</p>
+      </div>
+    );
+  }
+}
+
+export default Summary;
